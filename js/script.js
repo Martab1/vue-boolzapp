66a56ff464baff3bf793afadb6db5c9f99b3979b
Martab1 <martabernardo86@gmail.com>
2021-04-26 14:12:52 +0200

    set contact-chat

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 
+/*  
+Milestone 1
+Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse. I messaggi saranno statici per ora. Grafica e immagini allegate nello zip.
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto. I dati dei contatti li trovate allegati nello zip.
+
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato.
+
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+*/
+
+
+
 const app = new Vue({
     el: "#app",
     data:{
@@ -89,6 +108,16 @@ const app = new Vue({
             },
         ],
         userName: "Marta Bernardo" ,
+        contactIndex: 0,
+    
+    },
+    methods:{
+        // IMPOSTARE CONTATTO ATTRAVERSO L'INDEX
+        setContact(index){
+            this.contactIndex = index;
+            console.log(this.contactIndex);
+
+        }
     }
 
 });
